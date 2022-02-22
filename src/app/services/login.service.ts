dfbd4e34fd5c45148fc5167af2657ddb77aaed6a
darkenergy1856 <aryanchandra7360@gmail.com>
2022-02-22 19:22:19 +0530

    Some Minor Changes

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Register } from '../Entity/register';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { Doctor } from '../Entity/doctor';
 import { LoggedInUser } from '../Entity/logged-in-user';
 import { BehaviorSubject, Subject } from 'rxjs';
